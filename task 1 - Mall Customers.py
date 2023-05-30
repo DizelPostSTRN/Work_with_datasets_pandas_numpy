34cabd2f99fe4f7ae279b1f95c5615dd14a65904
DizelPostSTRN <ragno1@mail.ru>
2023-05-30 21:09:11 +0300

    v0.1

--- task 1 - Mall Customers.py
+++ task 1 - Mall Customers.py
@@ -2,6 +2,28 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+
 # from sklearn.cluster import KMeans
 
-print('123')
+
+# Сбор и анализ данных
+
+# 1. Загрузка данных из CSV-файла в Pandas DataFrame
+customer_data = pd.read_csv(
+    'C:/Users/devdi/PycharmProjects/Work_with_datasets_pandas_numpy/Datasets/Task-1 - Mall_Customers.csv')
+
+# Первые пять строк в датафрейме
+print(customer_data.head())
+
+# Найти количество строк и столбцов
+print(customer_data.shape)
+
+# Получение некоторой информации о наборе данных
+print(customer_data.info())
+
+# Проверка пропущенных значений
+print(customer_data.isnull().sum())
+
+# Выбор столбца «Годовой доход(Annual Income)» и столбца «Оценка расходов(Spending Score)»
+x = customer_data.iloc[:,[3,4]].values
+print(x)
